4fed7b89fd67119e93a89d3342102f3975637211
benreckas <benreckas@gmail.com>
2018-04-10 19:36:47 -0700

    Completed exercises

--- script.js
+++ script.js
@@ -1,1 +1,14 @@
 
+(function electricMucus() {
+
+  // Queries
+  const input = document.getElementById('textinputid');
+  const btn = document.getElementById('buttonid');
+
+  // Check input value, change bg color and validate to enable button
+  input.addEventListener('input', e => {
+    input.value.toLowerCase() === 'electric mucus' ? btn.style.backgroundColor = "lightgreen" : btn.style.backgroundColor = input.value;
+    input.value.toLowerCase() === 'electric mucus' ? btn.disabled = false : btn.disabled = true;
+  });
+
+})();
